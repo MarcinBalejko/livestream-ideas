9c332d76ad5e4d7648cb605ff9739e0cbc2be34a
MarcinBalejko <mbbm9@o2.pl>
2020-02-18 01:21:26 +0100

    :white_check_mark: Idea objects can be created

--- app.js
+++ app.js
@@ -4,6 +4,30 @@ const socketio = require('@feathersjs/socketio');
 const moment = require('moment');
 
 // Idea service
+class IdeaService {
+  constructor() {
+    this.ideas = [];
+  }
+
+  async find() {
+    return this.ideas;
+  }
+
+  async create(data) {
+    const idea = {
+      id: this.ideas.length,
+      text: data.text,
+      tech: data.tech,
+      viewer: data.viewer
+    };
+
+    idea.time = moment().format('h:mm:ss a');
+
+    this.ideas.push(idea);
+
+    return idea;
+  }
+}
 
 const app = express(feathers());
 
@@ -22,6 +46,15 @@ app.publish(data => app.channel('stream'));
 
 const PORT = process.env.PORT || 3030;
 
-app.listen(PORT).on('listening', () =>
-  console.log(`Realtime server running on port ${PORT}`)
-);
+app
+  .listen(PORT)
+  .on('listening', () =>
+    console.log(`Realtime server running on port ${PORT}`)
+  );
+
+app.service('ideas').create({
+  text: 'Build a cool app',
+  tech: 'Node.js',
+  viewer: 'John Doe',
+  time: moment().format('h:mm:ss a')
+});
